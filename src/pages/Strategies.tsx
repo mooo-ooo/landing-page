5e0cf22a6c51a0507e24df24f2e5d6c0f35a5555
Duy Huynh <huquduy@gmail.com>
2025-09-01 10:20:37 +0700

    feat: orderbot

--- src/pages/Strategies.tsx
+++ src/pages/Strategies.tsx
@@ -8,8 +8,11 @@ import {
   TableHead,
   TableRow,
   IconButton,
-  TableCell,
+  TableCell as TableCellMui,
+  Card,
+  LinearProgress,
 } from "@mui/material";
+import { styled } from "@mui/system";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import DriveFileRenameOutlineIcon from "@mui/icons-material/DriveFileRenameOutline";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
@@ -18,6 +21,9 @@ import { useDispatch } from "react-redux";
 import numeral from "numeral";
 import type { AppDispatch } from "../redux/store";
 import { useSnackbar } from "notistack";
+import { DndProvider, useDrag, useDrop } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
 import {
   setUpdateStrategy,
   setNewStrategy,
@@ -25,14 +31,27 @@ import {
 } from "../redux/strategy/strategySlice";
 import api from "../lib/axios";
 
+const ItemTypes = {
+  BOX: "box",
+};
+
+interface DropResult {
+  id: string;
+}
+
 const Strategies: FC = () => {
   const { enqueueSnackbar } = useSnackbar();
   const dispatch = useDispatch<AppDispatch>();
   const [strategies, setStrategies] = useState<IStrategy[]>([]);
+  const [bots, setBots] = useState<{ _id: string; strategyId: string }[]>([]);
 
   const fetchStrategies = () => {
     api.get("/api/v1/strategies").then(({ data }) => {
       setStrategies(data);
+    });
+
+    api.get("/api/v1/botorders").then(async ({ data }) => {
+      setBots(data);
     });
   };
   useEffect(() => {
@@ -46,130 +65,251 @@ const Strategies: FC = () => {
   };
   return (
     <Box display="flex" flexDirection="column" gap="12px" py="16px">
-      <Box display="flex" alignItems="center">
-        <Typography>Your Strategies</Typography>
+      <DndProvider backend={HTML5Backend}>
+        <Box mb={4}>
+          <Typography mb={2}>Your Bots</Typography>
+          <Box>
+            {bots.map((bot) => {
+              const strategy = strategies.find((strategy) => {
+                return strategy._id === bot.strategyId;
+              });
+              return <BotItem strategy={strategy} bot={bot} />;
+            })}
+          </Box>
+        </Box>
+        <Box display="flex" alignItems="center">
+          <Typography>Your Strategies</Typography>
+          <IconButton
+            onClick={() =>
+              dispatch(setNewStrategy({ open: true, baseToken: "" }))
+            }
+            size="small"
+            sx={{
+              color: "text.secondary",
+              "&:hover": { color: "primary.main" },
+            }}
+          >
+            <AddIcon />
+          </IconButton>
+        </Box>
+
+        <Paper
+          sx={{
+            width: "100%",
+            overflow: "hidden",
+            mb: 2,
+            backgroundColor: "#010409",
+            border: "1px solid #30363d",
+          }}
+        >
+          <Table>
+            <TableHead>
+              <TableRow sx={{ height: "48px" }}>
+                <TableCell align="left">
+                  <Typography color="textSecondary">Name</Typography>
+                </TableCell>
+                <TableCell align="left">
+                  <Typography color="textSecondary">Sell Exchange</Typography>
+                </TableCell>
+                <TableCell align="left">
+                  <Typography color="textSecondary">Buy Exchange</Typography>
+                </TableCell>
+                <TableCell align="left">
+                  <Typography color="textSecondary">
+                    Open Spread Rate
+                  </Typography>
+                </TableCell>
+                <TableCell align="left">
+                  <Typography color="textSecondary">
+                    Close Spread Rate
+                  </Typography>
+                </TableCell>
+                <TableCell align="left">
+                  <Typography color="textSecondary">
+                    Max vol per order
+                  </Typography>
+                </TableCell>
+                <TableCell align="left">
+                  <Typography color="textSecondary">Max vol</Typography>
+                </TableCell>
+                <TableCell align="left">
+                  <MoreVertIcon />
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {strategies.map((strategy) => (
+                <StrategyRow
+                  strategy={strategy}
+                  handleRemoveStrategy={handleRemoveStrategy}
+                  fetchStrategies={fetchStrategies}
+                />
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
+      </DndProvider>
+    </Box>
+  );
+};
+
+export default Strategies;
+
+const BotItem = ({
+  bot,
+  strategy,
+}: {
+  bot: { _id: string; strategyId: string };
+  strategy?: IStrategy;
+}) => {
+  const [{ canDrop, isOver }, drop] = useDrop(() => ({
+    accept: ItemTypes.BOX,
+    drop: () => ({ id: bot._id }),
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop(),
+    }),
+  }));
+  const isActive = canDrop && isOver;
+  return (
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    <Card
+      ref={drop}
+      sx={{
+        p: 2,
+        width: "30%",
+        border: isActive
+          ? "1px solid #ffa726"
+          : "1px solid #121212",
+      }}
+    >
+      <Typography color="textSecondary">Id: {bot._id}</Typography>
+      <Box height={6} />
+      <Typography>Strategy: {strategy?.strategyName}</Typography>
+      <Box height={6} />
+      {/* <Box sx={{ width: "100%" }}>
+        <LinearProgress
+          sx={{
+            height: 2,
+            backgroundColor: "rgb(6 84 53)",
+            "& .MuiLinearProgress-bar": {
+              backgroundColor: "rgb(14, 203, 129)",
+            },
+          }}
+        />
+      </Box> */}
+    </Card>
+  );
+};
+
+const StrategyRow = ({
+  strategy,
+  handleRemoveStrategy,
+  fetchStrategies,
+}: {
+  strategy: IStrategy;
+  handleRemoveStrategy: (id: string) => void;
+  fetchStrategies: () => void;
+}) => {
+  const {
+    strategyName,
+    sellExchange,
+    buyExchange,
+    sellSymbol,
+    buySymbol,
+    _id,
+    bestInSpread,
+    secondInSpread,
+    bestOutSpread,
+    secondOutSpread,
+    maxOrderVol,
+    maxVolOfPosition,
+  } = strategy;
+
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: ItemTypes.BOX,
+    item: { _id },
+    end: (item, monitor) => {
+      const dropResult = monitor.getDropResult<DropResult>();
+      if (item && dropResult) {
+        api
+          .put("/api/v1/botorders", {
+            _id: dropResult.id,
+            strategyId: item._id,
+          })
+          .then(async () => {
+            fetchStrategies();
+          });
+      }
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+      handlerId: monitor.getHandlerId(),
+    }),
+  }));
+  const baseToken = sellSymbol.split("/")[0];
+  const dispatch = useDispatch<AppDispatch>();
+
+  const opacity = isDragging ? 0.4 : 1;
+  return (
+    <TableRow key={_id} sx={{ opacity }}>
+      <TableCell>
+        <div
+          ref={drag}
+          style={{ cursor: "move", textAlign: "center", lineHeight: "36px" }}
+        >
+          {strategyName}
+        </div>
+      </TableCell>
+      <TableCell
+        sx={{
+          color: "rgb(246, 70, 93)",
+        }}
+      >
+        [{sellExchange.toUpperCase()}] {sellSymbol}
+      </TableCell>
+      <TableCell
+        sx={{
+          color: "rgb(14, 203, 129)",
+        }}
+      >
+        [{buyExchange.toUpperCase()}] {buySymbol}
+      </TableCell>
+      <TableCell>
+        {bestInSpread}% | {secondInSpread}%
+      </TableCell>
+      <TableCell>
+        {bestOutSpread}% | {secondOutSpread}%
+      </TableCell>
+      <TableCell>{maxOrderVol}</TableCell>
+      <TableCell>{numeral(maxVolOfPosition).format("0,0.[000]")}</TableCell>
+      <TableCell>
         <IconButton
-          onClick={() =>
-            dispatch(setNewStrategy({ open: true, baseToken: "" }))
-          }
+          onClick={() => dispatch(setUpdateStrategy({ open: true, baseToken }))}
           size="small"
           sx={{
             color: "text.secondary",
             "&:hover": { color: "primary.main" },
           }}
         >
-          <AddIcon />
+          <DriveFileRenameOutlineIcon />
         </IconButton>
-      </Box>
-
-      <Paper
-        sx={{
-          width: "100%",
-          overflow: "hidden",
-          mb: 2,
-          backgroundColor: "#010409",
-          border: "1px solid #30363d",
-        }}
-      >
-        <Table>
-          <TableHead
-            sx={{
-              position: "sticky",
-              top: 0,
-              zIndex: 1,
-              backgroundColor: "#010409",
-            }}
-          >
-            <TableRow sx={{ height: "48px" }}>
-              <TableCell align="left">Name</TableCell>
-              <TableCell align="left">Sell Exchange</TableCell>
-              <TableCell align="left">Buy Exchange</TableCell>
-              <TableCell align="left">Open Spread Rate</TableCell>
-              <TableCell align="left">Close Spread Rate</TableCell>
-              <TableCell align="left">Max vol per order</TableCell>
-              <TableCell align="left">Max vol</TableCell>
-              <TableCell align="left">
-                <MoreVertIcon />
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {strategies.map(
-              ({
-                strategyName,
-                sellExchange,
-                buyExchange,
-                sellSymbol,
-                buySymbol,
-                _id,
-                bestInSpread,
-                secondInSpread,
-                bestOutSpread,
-                secondOutSpread,
-                maxOrderVol,
-                maxVolOfPosition,
-              }) => {
-                const baseToken = sellSymbol.split("/")[0];
-                return (
-                  <TableRow key={_id}>
-                    <TableCell>{strategyName}</TableCell>
-                    <TableCell
-                      sx={{
-                        color: "rgb(246, 70, 93)",
-                      }}
-                    >
-                      [{sellExchange.toUpperCase()}] {sellSymbol}
-                    </TableCell>
-                    <TableCell
-                      sx={{
-                        color: "rgb(14, 203, 129)",
-                      }}
-                    >
-                      [{buyExchange.toUpperCase()}] {buySymbol}
-                    </TableCell>
-                    <TableCell>
-                      {bestInSpread}% | {secondInSpread}%
-                    </TableCell>
-                    <TableCell>
-                      {bestOutSpread}% | {secondOutSpread}%
-                    </TableCell>
-                    <TableCell>{maxOrderVol}</TableCell>
-                    <TableCell>
-                      {numeral(maxVolOfPosition).format("0,0.[000]")}
-                    </TableCell>
-                    <TableCell>
-                      <IconButton
-                        onClick={() =>
-                          dispatch(setUpdateStrategy({ open: true, baseToken }))
-                        }
-                        size="small"
-                        sx={{
-                          color: "text.secondary",
-                          "&:hover": { color: "primary.main" },
-                        }}
-                      >
-                        <DriveFileRenameOutlineIcon />
-                      </IconButton>
-                      <IconButton
-                        onClick={() => handleRemoveStrategy(_id)}
-                        size="small"
-                        sx={{
-                          color: "text.secondary",
-                          "&:hover": { color: "primary.main" },
-                        }}
-                      >
-                        <DeleteForeverIcon />
-                      </IconButton>
-                    </TableCell>
-                  </TableRow>
-                );
-              }
-            )}
-          </TableBody>
-        </Table>
-      </Paper>
-    </Box>
+        <IconButton
+          onClick={() => handleRemoveStrategy(_id)}
+          size="small"
+          sx={{
+            color: "text.secondary",
+            "&:hover": { color: "primary.main" },
+          }}
+        >
+          <DeleteForeverIcon />
+        </IconButton>
+      </TableCell>
+    </TableRow>
   );
 };
 
-export default Strategies;
+const TableCell = styled(TableCellMui)(() => ({
+  padding: "6px 16px",
+}));
